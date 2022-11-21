34b221474b3336a74145cd7219764d287b143923
gustavopgh <gustavohenriquehed@gmail.com>
2022-11-21 19:30:16 -0300

    sessões

---
+++ 21_11_2022/sessao_id.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Pagina WEB - Sessão de ID</title>
+</head>
+<body>
+
+	<?php 	
+
+		session_start();
+			 
+			 ?>
+
+			 <h3>Sessão de Usuário</h3>
+
+			 Sid desta sessão é:<?php 	echo session_id(); ?>
+
+
+</body>
+</html>
---
+++ 21_11_2022/sessao_usuario.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Página WEB - Sessão de Uduário</title>
+</head>
+<body>
+	<?php 
+
+			session_start();
+
+			$_SESSION['NOME'] = "Geraldo"
+
+	 ?>
+
+	 <h3>Sessão de Usuário</h3>
+	 Bem vindo, Sr. <?php echo $_SESSION['NOME']; ?>
+
+</body>
+</html>
